4c1e375e4453f430ff1f4422e5a27cbcf013e834
New Author Name <dennise.e.camacho@gmail.com>
2022-07-09 23:13:27 -0500

    figure out test error...

--- codewars_tests.js
+++ codewars_tests.js
@@ -1,18 +1,24 @@
-describe("this function called toCamelCase", function () {
+describe("the function toCamelCase", function () {
     it("should be a defined function", function () {
         expect(typeof toCamelCase).toBe("function");
     });
 });
 
-describe("this function called solution", function () {
+describe("the function solution", function () {
     it("should be a defined function", function () {
         expect(typeof solution).toBe("function");
     });
 });
 
-describe("this function called digitize", function () {
+describe("the function digitize", function () {
     it("should be a defined function", function () {
         expect(typeof digitize).toBe("function");
     });
+
+    it("should return an array when called", function () {
+        expect(typeof digitize()).toBe("object");
+    });
+//    TypeError: Cannot read properties of undefined (reading 'reverse')
+
 });
 
